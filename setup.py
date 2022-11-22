6dd9b75b28cf2578cc73dd83b3a14af206f44b3a
Daniel Tang <tang.d@hotmail.com>
2022-11-22 22:46:40 +1100

    Update setup.py for the spiir.data namespace package

--- setup.py
+++ setup.py
@@ -1,31 +1,22 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import find_namespace_packages, setup
 
 setup(
-    name="spiir",
+    name="spiir-data",
     version="0.0.2",
-    packages=find_namespace_packages(where="src", include=["spiir.*"]),
     package_dir={"": "src"},
+    packages=find_namespace_packages(where="src", include=["spiir*"]),
+    namespace_packages=["spiir"],
     python_requires=">=3.8",
     install_requires=[
-        "wheel",
-        "setuptools",
         "lalsuite",
         "astropy",
         "python-ligo-lw>=1.8.1",
-        "ligo.skymap",
-        "igwn-alert",
-        "ligo-gracedb",
-        "toml",
-        "numpy",
+        "numpy>=1.23",
         "scipy",
         "pandas",
         "matplotlib",
-        "click",
     ],
-    extras_require={
-        "pycbc": ["pycbc"],
-    },
-    description="A Python library for the SPIIR gravitational wave search pipeline.",
+    description="A Python data library for gravitational wave science by SPIIR.",
     author="Daniel Tang",
     author_email="daniel.tang@uwa.edu.au",
 )
